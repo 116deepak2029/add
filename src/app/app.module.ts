0f701ff9e47d407f8fe7ee8c3a8306838988a917
deepak <deepakswaminath@gmail.com>
2022-04-05 10:33:38 +0530

    Intial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TemplateComponent } from './template/template.component';
+import { SliderComponent } from './slider/slider.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    TemplateComponent,
+    SliderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
